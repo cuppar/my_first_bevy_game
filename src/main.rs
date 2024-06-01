93b243b47528447b9c9a4cba695abd5ca4801363
cuppar <cuppar@163.com>
2024-06-02 00:15:15 +0800

    sprite sheet and sprite display

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,15 @@ use bevy::window::close_on_esc;
 
 const WW: f32 = 1200.0;
 const WH: f32 = 900.0;
-const BG_COLOR: (u8, u8, u8) = (25, 20, 43);
+const BG_COLOR: (u8, u8, u8) = (197, 204, 183);
+
+// Sprites
+const SPRITE_SHEET_PATH: &str = "sheet.png";
+const SPRITE_SCALE_FACTOR: f32 = 3.0;
+const SPRITE_SHEET_W: usize = 32;
+const SPRITE_SHEET_H: usize = 32;
+const TILE_W: usize = 24;
+const TILE_H: usize = 24;
 
 fn main() {
     App::new()
@@ -27,11 +35,43 @@ fn main() {
         .insert_resource(Msaa::Off)
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(FrameTimeDiagnosticsPlugin)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, spawn_player))
         .add_systems(Update, close_on_esc)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    let texture = asset_server.load(SPRITE_SHEET_PATH);
+    let layout = TextureAtlasLayout::from_grid(
+        Vec2::new(TILE_W as f32, TILE_H as f32),
+        SPRITE_SHEET_W,
+        SPRITE_SHEET_H,
+        None,
+        None,
+    );
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+
+    commands.spawn((
+        SpriteSheetBundle {
+            texture,
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 11,
+            },
+            transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+            ..default()
+        },
+        Player,
+    ));
 }
