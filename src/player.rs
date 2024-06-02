2ff71b0bf613094585ccb4db9dd1a0d177a119ee
cuppar <cuppar@163.com>
2024-06-03 00:12:41 +0800

    gun follow mouse position and around with player

--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,15 @@
+use bevy::math::vec2;
 use bevy::prelude::*;
 
 use crate::movement::Velocity;
 use crate::prelude::*;
+use crate::resource::CursorPosition;
 use crate::state::GameState::InGame;
 
 const PLAYER_MOVE_SPEED: f32 = 200.;
 const PLAYER_GUN_Z_INDEX: usize = 1;
+const PLAYER_GUN_DISTANCE_OFFSET: f32 = 20.;
+const PLAYER_GUN_POS_OFFSET: Vec2 = vec2(0., -30.);
 
 pub struct PlayerPlugin;
 
@@ -52,7 +56,7 @@ pub fn spawn_player_gun(mut commands: Commands, sprite_sheet: Res<SpriteSheet>) 
                 layout: sprite_sheet.layout.clone().unwrap(),
                 index: 20,
             },
-            transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR))
+            transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR / 1.5))
                 .with_translation(Vec2::ZERO.extend(PLAYER_GUN_Z_INDEX as f32)),
             ..default()
         },
@@ -73,6 +77,7 @@ fn handle_player_movement_input(
     let down = button_input.pressed(KeyCode::ArrowDown) || button_input.pressed(KeyCode::KeyS);
     let left = button_input.pressed(KeyCode::ArrowLeft) || button_input.pressed(KeyCode::KeyA);
     let right = button_input.pressed(KeyCode::ArrowRight) || button_input.pressed(KeyCode::KeyD);
+
     if up {
         direction.y += 1.;
     }
@@ -85,20 +90,68 @@ fn handle_player_movement_input(
     if right {
         direction.x += 1.;
     }
+
     let mut player_velocity = query.single_mut();
+
     player_velocity.direction = direction.normalize_or_zero();
 }
 
 fn gun_follow_player(
+    cursor_position: Res<CursorPosition>,
     player_query: Query<&Transform, With<Player>>,
     mut gun_query: Query<&mut Transform, (With<Gun>, Without<Player>)>,
 ) {
     if player_query.is_empty() || gun_query.is_empty() {
         return;
     }
-    let player_translation = player_query.single().translation;
-    let mut gun_transform = gun_query.single_mut();
 
-    // 只移动xy平面,保持z-index不变
-    gun_transform.translation = player_translation.xy().extend(gun_transform.translation.z);
+    let mut gun_transform = gun_query.single_mut();
+    let gun_pos = gun_transform.translation.xy();
+
+    let cursor_pos = cursor_position.unwrap_or(gun_pos);
+    let gun_to_cursor = cursor_pos - gun_pos;
+
+    // 3d absolute
+    if gun_to_cursor.length() > 0. {
+        gun_transform.rotation =
+            Quat::from_rotation_arc(Vec3::Y, gun_to_cursor.extend(0.).normalize());
+    } else {
+        gun_transform.rotation = Quat::default();
+    }
+
+    let player_pos = player_query.single().translation.xy();
+
+    let new_gun_pos = player_pos
+        + PLAYER_GUN_POS_OFFSET
+        + gun_transform.local_y().xy().normalize() * PLAYER_GUN_DISTANCE_OFFSET;
+
+    gun_transform.translation = new_gun_pos.extend(gun_transform.translation.z);
+
+    // 2d absolute
+    // let mut absolute_angle = Vec2::Y.angle_between(gun_to_cursor);
+    // if absolute_angle.is_nan() {
+    //     absolute_angle = 0.;
+    // }
+    // let mut rotated_gun_transform =
+    //     Transform::from_translation(gun_transform.translation).with_scale(gun_transform.scale);
+    // rotated_gun_transform.rotate_z(absolute_angle);
+    // *gun_transform = rotated_gun_transform;
+
+    // 2d relative
+    // let mut relative_angle = gun_transform.local_y().xy().angle_between(gun_to_cursor);
+    // if relative_angle.is_nan() {
+    //     relative_angle = 0.;
+    // }
+    // gun_transform.rotate_z(relative_angle);
+
+    // 3d relative
+    // if gun_to_cursor.length() > 0. {
+    //     let rotation = Quat::from_rotation_arc(
+    //         gun_transform.local_y().xyz(),
+    //         gun_to_cursor.extend(0.).normalize(),
+    //     );
+    //     gun_transform.rotate_local(rotation);
+    // } else {
+    //     gun_transform.rotation = Quat::default();
+    // }
 }
--- src/resource.rs
+++ src/resource.rs
@@ -1,4 +1,7 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+use crate::state::GameState::InGame;
 
 const SPRITE_SHEET_PATH: &str = "sheet.png";
 const SPRITE_SHEET_W: usize = 32;
@@ -12,10 +15,15 @@ pub struct SpriteSheet {
     pub layout: Option<Handle<TextureAtlasLayout>>,
 }
 
+#[derive(Resource, Default, Deref, DerefMut, Debug)]
+pub struct CursorPosition(Option<Vec2>);
+
 pub struct ResourcePlugin;
 impl Plugin for ResourcePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<SpriteSheet>();
+        app.init_resource::<SpriteSheet>()
+            .init_resource::<CursorPosition>()
+            .add_systems(Update, update_cursor_position.run_if(in_state(InGame)));
     }
 }
 
@@ -33,4 +41,18 @@ pub fn load_sprite_sheet(
         None,
     );
     sprite_sheet.layout = Some(texture_atlas_layout.add(layout));
+}
+
+fn update_cursor_position(
+    mut cursor_position: ResMut<CursorPosition>,
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform), With<Camera>>,
+) {
+    let window = q_window.single();
+    let (camera, camera_global_transform) = q_camera.single();
+
+    **cursor_position = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_global_transform, cursor))
+        .map(|ray| ray.origin.truncate());
 }
